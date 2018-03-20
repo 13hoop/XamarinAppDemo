43cbb4558c53286d53e5939b0ebefef46150d193
13hoop <zhanxin13@gmail.com>
2018-03-20 09:57:54 +0800

    init project

---
+++ VSAppTest/MyViewController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using UIKit;
+
+namespace VSAppTest
+{
+    public partial class MyViewController : UIViewController
+    {
+        public MyViewController() : base("MyViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            setUpViews();
+        }
+
+        void setUpViews() {
+            base.View.BackgroundColor = UIColor.Magenta;
+        }
+    }
+}
+
---
+++ VSAppTest/ViewController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using UIKit;
+
+namespace VSAppTest
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            setUpViews();
+        }
+
+        void setUpViews()
+        {
+            base.View.BackgroundColor = UIColor.Magenta;
+
+
+
+        }
+
+        UIButton Button {
+            get;
+        }
+    }
+}
